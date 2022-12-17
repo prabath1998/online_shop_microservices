02c42dce979b0c50663dfe1114a8e3cb34216c3c
prabath1998 <prabhathudayanga1998@gmail.com>
2022-12-17 15:41:11 +0530

    add product service

---
+++ product-service/src/main/java/com/test/productservice/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.test.productservice.service;
+
+import com.test.productservice.dto.ProductRequest;
+import com.test.productservice.dto.ProductResponse;
+import com.test.productservice.model.Product;
+import com.test.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    @Autowired
+    private final ProductRepository productRepository;
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+        log.info("Product {} is saved successfully",product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+       List<Product> products = productRepository.findAll();
+       return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
